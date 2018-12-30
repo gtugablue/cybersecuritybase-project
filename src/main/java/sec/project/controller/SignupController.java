b13990fcf71bc659de4cc1a0bce1a29381d3265f
Gustavo Silva <silva95gustavo@gmail.com>
2018-12-30 15:21:28 +0000

    Broken Access Control, XSS and Insufficient Logging

--- src/main/java/sec/project/controller/SignupController.java
+++ src/main/java/sec/project/controller/SignupController.java
@@ -2,11 +2,15 @@ package sec.project.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import sec.project.domain.Signup;
 import sec.project.repository.SignupRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 public class SignupController {
@@ -27,7 +31,13 @@ public class SignupController {
     @RequestMapping(value = "/form", method = RequestMethod.POST)
     public String submitForm(@RequestParam String name, @RequestParam String address) {
         signupRepository.save(new Signup(name, address));
-        return "done";
+        return "redirect:/done";
     }
 
+    @RequestMapping(value="/done", method = RequestMethod.GET)
+    public String done(Model model) {
+        List<String> participants = signupRepository.findAll().stream().map(signup -> signup.getName() + " &#45; " + signup.getAddress()).collect(Collectors.toList());
+        model.addAttribute("participants", participants);
+        return "done";
+    }
 }
